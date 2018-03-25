c50126ee732b24f8c495a41a72b1b6b7cb15bae0
Oppodelldog <odog@netcologne.de>
2018-03-25 15:41:08 +0200

    add simple example code and set language level to php7.2 in php storm

---
+++ Calculator.php
@@ -0,0 +1,9 @@
+<?php
+
+class Calculator
+{
+    public static function add(int $a, int $b): int
+    {
+        return $a + $b;
+    }
+}
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once 'Calculator.php';
+
+echo Calculator::add(12,22);
